a719deb1ea5082affc8eb16137ec3327b686f812
ApurboStarry <hasinapurbo984@gmail.com>
2021-04-13 16:10:49 +0600

    optimized for playing videos on lg smart tv

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ app.use(cors());
 
 require("./startup/routes")(app);
 
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/lg", (req, res) => {
+	res.sendFile(
+    "/media/apurbo/A69A97279A96F353/Procrastination/MCU/Captain America Civil War (2016) [1080p] [YTS.AG]/Captain.America.Civil.War.2016.1080p.BluRay.x264-[YTS.AG].mp4"
+  );
+});
+
 const portNumber = 8000;
 app.listen(portNumber, function () {
   console.log(`Server is running on http://${ip.address() + ":" + portNumber}`);
